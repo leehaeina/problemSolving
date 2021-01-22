93fca04b81247a81bca815bb2cc5c48d307c522f
leehaeina <leehaein0164@gmail.com>
2021-01-22 15:53:17 +0900

    큰수만들기

---
+++ programmers/lv.2/큰수만들기.js
@@ -0,0 +1,18 @@
+function solution(number, k) {
+    var answer = '';
+    let start = 0;
+    let end = number.length - k;
+    let arr = number.split('').map(v=>parseInt(v));
+    for(let i = (arr.length-k);i>0;i--)
+    {
+        let sliced = arr.slice(start,end+1);
+        let max = Math.max.apply(null,sliced);
+        let index = sliced.indexOf(max) + 1;
+        start = start + index;
+        end = arr.length -i+1;
+        answer += max;
+    }
+    return answer;
+}
+
+console.log(solution("4177252841",4));
